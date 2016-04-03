39342d9aa0501574225908b2256aa44036a46f4d
Zachary Jaghory <zj@zach-jaghory.com>
2016-04-02 23:01:30 -0400

    Fixed problem with multiple select

--- resources/views/fields/select.blade.php
+++ resources/views/fields/select.blade.php
@@ -1,6 +1,10 @@
 <div class="form-group">
     <label for="{{$select_field->get('id')}}">{{$select_field->get('name')}}</label>
-    <select class="form-control" name="{{$select_field->get('id')}}" id="{{$select_field->get('id')}}">
+    @if(json_decode($select_field->get('options'))->multipleselect == true)
+        <select multiple class="form-control" name="{{$select_field->get('id')}}" id="{{$select_field->get('id')}}">
+    @else
+        <select class="form-control" name="{{$select_field->get('id')}}" id="{{$select_field->get('id')}}">
+    @endif
         @foreach(json_decode($select_field->get('options'))->options as $option)
             <option value="{{$option->value}}">{{$option->label}}</option>
         @endforeach
