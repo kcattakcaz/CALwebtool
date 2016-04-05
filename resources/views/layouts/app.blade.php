1cfe9a2979fdb9dac239945b0fc95ff7b1690456
Matthew <yoonmatt@msu.edu>
2016-04-05 12:11:14 -0400

    resources>assets>css...added css and less

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ elixir('custom/app.custom') }}" rel="stylesheet"> --}}
     <style>
         body {
             font-family: 'Lato';
