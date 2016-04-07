5fcac795ea03f0fa088d063f23723f3b0767b29e
Matthew <yoonmatt@msu.edu>
2016-04-07 00:58:15 -0400

    gulped less file implementing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/public/css/custom-variables.css" rel="stylesheet">
     {{-- <link href="{{ elixir('custom/app.custom') }}" rel="stylesheet"> --}}
     <style>
         body {
