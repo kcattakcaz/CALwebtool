1e72518095362118fc042429620493af78539534
Zachary Jaghory <zj@zach-jaghory.com>
2016-04-09 19:38:06 -0400

    Crappy multiple select view

---
+++ resources/views/fields/submission/select.blade.php
@@ -0,0 +1,17 @@
+
+@if(json_decode($select_field->get('fieldDef')->options)->multipleselect == false)
+    <div class="input-group">
+        <span class="input-group-addon" id="basic-addon1">{{$select_field->get('fieldDef')->name}}</span>
+        <input disabled type="text" class="form-control" aria-describedby="basic-addon1" value="{{$select_field->get('submission')}}">
+    </div>
+@else
+    <div class="input-group">
+        <span class="input-group-addon" id="basic-addon1">{{$select_field->get('fieldDef')->name}}</span>
+        <input disabled type="text" class="form-control" aria-describedby="basic-addon1" value="
+@foreach($select_field->get('submission') as $opt)
+{{$opt}} | 
+@endforeach
+
+                ">
+    </div>
+@endif
